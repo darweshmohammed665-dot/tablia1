719f06839817cdb0cd16100b8422e00cf3b3adac
YASMEEN-IT <yasmeenabdelaziz36@gmail.com>
2026-04-12 23:42:20 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,4 @@
-
-  // Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-analytics.js";
   // TODO: Add SDKs for Firebase products that you want to use
